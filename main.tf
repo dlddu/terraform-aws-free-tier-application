14786d5fb3968bb4d41302c74f71512f4f2e3002
dlddu <39251873+dlddu@users.noreply.github.com>
2024-09-07 11:37:24 +0000

    feat: Initial commit

---
+++ main.tf
@@ -0,0 +1,56 @@
+module "free_tier" {
+  source  = "dlddu/free-tier/aws"
+  version = "0.0.2"
+
+  account_alias            = var.account_alias
+  budget_description       = "${var.application_name} - free tier"
+  subscriber_email_address = var.subscriber_email_address
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = var.ssh_key_name
+  public_key = var.public_key
+}
+
+resource "aws_security_group" "this" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "this" {
+  ami                         = "ami-05d2438ca66594916"
+  instance_type               = "t2.micro"
+  key_name                    = aws_key_pair.this.key_name
+  vpc_security_group_ids      = [aws_security_group.this.id]
+  user_data_replace_on_change = true
+  user_data                   = var.user_data
+}
+
+output "public_ip" {
+  value = aws_instance.this.public_ip
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "account_alias" {
+  type = string
+}
+
+variable "application_name" {
+  type = string
+}
+
+variable "subscriber_email_address" {
+  type = string
+}
+
+variable "ssh_key_name" {
+  type = string
+}
+
+variable "public_key" {
+  type = string
+}
+
+variable "domain" {
+  type = string
+}
+
+variable "user_data" {
+  type = string
+}
